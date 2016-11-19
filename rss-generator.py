188c52f0f6e9ad85c87c08fa5a6790554df16c9a
Ibrahim Jarif <jarifibrahim@gmail.com>
2016-11-20 00:43:16 +0530

    Add description to RSS

--- rss-generator.py
+++ rss-generator.py
@@ -13,24 +13,37 @@ def generateFeed(urls):
         fe = fg.add_entry()
         fe.title(url[0])
         fe.link({'href': url[1], 'rel':'alternate'})
+        fe.description(url[2])
     print fg.rss_str(pretty=True)
-    # Write to file
+    # Write rss feed to file
     #fg.rss_file('rss.xml')
 
 def google_search(query):
     ''' Search google for the query and return set of urls
     Returns: urls (list)
-            [[Tile1,url1], [Title2, url2],..]
+            [[Title1, url1, desc1],
+             [Title2, url2, desc2],..]
     '''
     urls = []
     response = get_results_page(query)
-    soup = BeautifulSoup(response.read(), 'html5lib')   # using html5lib parser 
-    # Search for all relevant 'a' tags
-    for a in soup.select('.r a'):
-        parsed_url = urlparse.urlparse(a['href'])
+    soup = BeautifulSoup(response.read(), 'html5lib')   # using html5lib parser
+    # Search for all relevant 'div' tags with class 'g'
+    for div in soup.find_all('div',class_='g'):
+        # Search for 'span' containing the short description
+        desc = div.find_all('span', class_='st')
+        # Search for 'a' containing the link
+        anchorTag = div.select_one('.r a')
+        # Skip if invalid div (it does not contain required a tag)
+        if not anchorTag:
+            continue
         # Validate url
+        parsedUrl = urlparse.urlparse(anchorTag['href'])
         if 'url' in parsed_url.path:
-            urls.append([a.text, str(urlparse.parse_qs(parsed_url.query)['q'][0])])
+            urlEntry = []
+            urlEntry.append(anchorTag.text)
+            urlEntry.append(urlparse.parse_qs(parsedUrl.query)['q'][0])
+            urlEntry.append(desc[0].text)
+            urls.append(urlEntry)
     return urls
 
 def get_results_page(query):
