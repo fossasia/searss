b25040e381a794e9b013dda9da4268e72b8f9f58
Lakshay Kalbhor <lakshay@Lakshays-MacBook-Pro.local>
2017-01-04 00:17:20 +0530

    Added argparse, followed pep8, added ascii art

--- rss-generator.py
+++ rss-generator.py
@@ -1,15 +1,35 @@
 
+DESC = """
+The goal of this mini-tool is gather search results and store it as an RSS feed on a server.
+"""
+ASCII = """
+ _______  _______  _______  ______    _______  _______ 
+|       ||       ||   _   ||    _ |  |       ||       |
+|  _____||    ___||  |_|  ||   | ||  |  _____||  _____|
+| |_____ |   |___ |       ||   |_||_ | |_____ | |_____ 
+|_____  ||    ___||       ||    __  ||_____  ||_____  |
+ _____| ||   |___ |   _   ||   |  | | _____| | _____| |
+|_______||_______||__| |__||___|  |_||_______||_______|
+
+"""
+
 import mechanize
 from feedgen.feed import FeedGenerator
 import urlparse
 import requests
 from bs4 import BeautifulSoup
 import sys
+import argparse
+from argparse import RawTextHelpFormatter
 
 
-google_feed = ("GOOGLE SEARCH RESULTS", "htps://www.google.com", "Google search results for %s")
-duckduckgo_feed = ("DUCKDUCKGO SEARCH RESULTS", "htps://www.duckduckgo.com", "Duckduckgo search results for %s")
-bing_feed = ("BING SEARCH RESULTS", "https://www.bing.com", "Bing search results for %s")
+google_feed = ("GOOGLE SEARCH RESULTS", "htps://www.google.com",
+               "Google search results for %s")
+duckduckgo_feed = ("DUCKDUCKGO SEARCH RESULTS",
+                   "htps://www.duckduckgo.com", "Duckduckgo search results for %s")
+bing_feed = ("BING SEARCH RESULTS", "https://www.bing.com",
+             "Bing search results for %s")
+
 
 def generateFeed(urls, query, search_engine):
     """
@@ -31,8 +51,8 @@ def generateFeed(urls, query, search_engine):
 
     fg = FeedGenerator()
     fg.title(feed[0])
-    fg.link(href = feed[1], rel='alternate')
-    fg.description(feed[2]%query)
+    fg.link(href=feed[1], rel='alternate')
+    fg.description(feed[2] % query)
 
     for url in urls:
         fe = fg.add_entry()
@@ -42,6 +62,7 @@ def generateFeed(urls, query, search_engine):
     print fg.rss_str(pretty=True)
     # Write rss feed to file
     # fg.rss_file('rss.xml')
+
 
 def get_results_page(query):
     """
@@ -58,6 +79,7 @@ def get_results_page(query):
     br.form['q'] = query
     return br.submit()
 
+
 def get_duckduckgo_page(query):
     """
     Fetch the duckduckgo search results page
@@ -72,7 +94,8 @@ def get_duckduckgo_page(query):
     br.select_form(name='x')
     br.form['q'] = query
     return br.submit()
-    
+
+
 def get_bing_page(query):
     """
     Fetch the bing search results page
@@ -91,6 +114,7 @@ def get_bing_page(query):
     br.select_form(nr=formcount)
     br.form['q'] = query
     return br.submit()
+
 
 def google_search(query):
     """
@@ -134,16 +158,17 @@ def duckduckgo_search(query):
     response = get_duckduckgo_page(query)
     soup = BeautifulSoup(response.read(), 'html5lib')
     # Search for all relevant 'div' tags with having the results
-    for div in soup.findAll('div', attrs = {'class' : ['result', 'results_links', 'results_links_deep', 'web-result']}):
-       # search for title
-       title = div.h2.text.replace("\n",'').replace("  ","")
-       # get anchor tag having the link
-       url = div.h2.a['href']
-       # get the short description
-       desc = div.find('a',attrs={'class':'result__snippet'}).text
-       url_entry = [title, url, desc]
-       urls.append(url_entry)
+    for div in soup.findAll('div', attrs={'class': ['result', 'results_links', 'results_links_deep', 'web-result']}):
+        # search for title
+        title = div.h2.text.replace("\n", '').replace("  ", "")
+        # get anchor tag having the link
+        url = div.h2.a['href']
+        # get the short description
+        desc = div.find('a', attrs={'class': 'result__snippet'}).text
+        url_entry = [title, url, desc]
+        urls.append(url_entry)
     return urls
+
 
 def bing_search(query):
     """
@@ -156,48 +181,87 @@ def bing_search(query):
     urls = []
     response = get_bing_page(query)
     soup = BeautifulSoup(response.read(), 'html5lib')
-    
+
     # Search for all relevant 'div' tags with having the results
-    for li in soup.findAll('li', attrs = {'class' : ['b_algo']}):
-       # search for title
-       title = li.h2.text.replace("\n",'').replace("  ","")
-       # get anchor tag having the link
-       url = li.h2.a['href']
-       # get the short description
-       desc = li.find('p').text
-       url_entry = [title, url, desc]
-       urls.append(url_entry)
+    for li in soup.findAll('li', attrs={'class': ['b_algo']}):
+        # search for title
+        title = li.h2.text.replace("\n", '').replace("  ", "")
+        # get anchor tag having the link
+        url = li.h2.a['href']
+        # get the short description
+        desc = li.find('p').text
+        url_entry = [title, url, desc]
+        urls.append(url_entry)
     return urls
 
+
 def main():
-    if len(sys.argv) == 2:
-        print("Wrong number of arguments")
-        exit(0)
-    if len(sys.argv) >= 3:
-        search_engine = int(sys.argv[1])
-        if search_engine not in [0,1]:
-            print("Wrong argument")
-            exit(0)
-        query = ' '.join(sys.argv[2:])
-    else:
-        search_engine = int(raw_input("Select the search engine (0 for google / 1 for duckduckgo / 2 for bing): "))
+
+    """
+    A smart argument parser to efficiently sort arguments 
+    and raise errors properly. Argument parser is scalable.
+    """
+
+    parser = argparse.ArgumentParser(
+        description='\n{}\n{}\n'.format(ASCII,DESC), formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--google', action='store_true',
+                        help='Set search engine as Google')
+    parser.add_argument('--bing', action='store_true',
+                        help='Set search engine as Bing')
+    parser.add_argument('--duckduckgo', action='store_true',
+                        help='Set search engine as DuckDuckGo')
+    parser.add_argument('-q','--query', action='store', dest='query',
+                        help='Specify search query. eg : --query "xkcd comics"')
+    args = parser.parse_args()
+
+    if args.google: #If Google is passed as argument
+        if args.query: #If query is passed as argument
+            query = args.query
+            urls = google_search(query)
+        else:
+            query = raw_input("What do you want to search for ? >> ")
+            urls = google_search(query)
+        generateFeed(urls, query, 0)
+
+    elif args.duckduckgo: #If DuckDuckGo is passed as argument
+        if args.query: #If query is passed as argument
+            query = args.query
+            urls = duckduckgo_search(query)
+        else:
+            query = raw_input("What do you want to search for ? >> ")
+            urls = duckduckgo_search(query)
+        generateFeed(urls, query, 1)
+
+    elif args.bing: #If Bing is passed as argument
+        if args.query: #If query is passed as argument
+            query = args.query
+            urls = bing_search(query)
+        else:
+            query = raw_input("What do you want to search for ? >> ")
+            urls = bing_search(query)
+        generateFeed(urls, query, 2)
+
+    else: #If no arguments or wrong arguments are passed
+        search_engine = int(raw_input(
+            "Select the search engine (0 for google / 1 for duckduckgo / 2 for bing): "))
         if search_engine not in [0, 1, 2]:
             print("Wrong choice. Please enter a valid choice.")
             main()
 
         query = raw_input("What do you want to search for ? >> ")
 
-    # google search
-    if search_engine == 0:
-        urls = google_search(query)
-        generateFeed(urls, query, search_engine)
-    # duckduckgo search
-    elif search_engine == 1:
-        urls = duckduckgo_search(query)
-        generateFeed(urls, query, search_engine)
-    elif search_engine == 2:
-        urls = bing_search(query)
-        generateFeed(urls, query, search_engine)
+        
+        if search_engine == 0:  # Google Search
+            urls = google_search(query)
+            generateFeed(urls, query, search_engine)
+        
+        elif search_engine == 1: # DuckDuckGo Search
+            urls = duckduckgo_search(query)
+            generateFeed(urls, query, search_engine)
+
+        elif search_engine == 2: # Bing Search
+            urls = bing_search(query)
+            generateFeed(urls, query, search_engine)
 
 
 if __name__ == "__main__":
